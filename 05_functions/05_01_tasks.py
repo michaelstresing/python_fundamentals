b6c857d8b29f459acc59e47275450f871508ddb1
michaelstresing <mstresing@gmail.com>
2019-07-13 00:54:29 +0100

    First take at section 5

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -14,3 +14,57 @@ Write a script that completes the following tasks.
 # calls a function that determines whether the number is divisible by 4 or 7 exclusively
 
 # print the results
+
+userput = int(input("Please enter a number between 1 and 1,000,000,000: "))
+
+def divby4and7(int):
+    '''
+    This checks if divisible by 4 and 7
+    :param int: must be a number
+    :return: yes or no
+    '''
+    if int % 4 != 0:
+        function1 = False
+    elif int % 7 != 0:
+        function1 = False
+    else:
+        function1 = True
+
+    return(f"It's {function1}, {userput} is divisible by both 4 and 7")
+
+def divby4or7(int):
+    '''
+    This checks if divisible by 4 or 7
+    :param int: must be a number
+    :return: yes or no
+    '''
+    if int % 4 == 0:
+        function2 = True
+    if int % 7 == 0:
+        function2 = True
+    else:
+        function2 = False
+
+    return(f"It's {function2}, {userput} is divisible by 4 or 7")
+
+def divby4or7ex(int):
+    '''
+    This checks if divisible by 4 or 7 exclusively
+    :param int: must be a number
+    :return: yes or no
+    '''
+    if int % 4 == 0:
+        if int % 7 != 0:
+            function3 = True
+        if int % 7 == 0:
+            function3 = False
+
+    if int % 7 == 0:
+        if int % 4 != 0:
+            function3 = True
+        if int % 4 == 0:
+            function3 = False
+
+    return(f"It's {function3}, {userput} is divisible by 4 and 7 exclusively")
+
+print(divby4and7(userput), "\n", divby4or7(userput), "\n", divby4or7ex(userput))
--- 05_functions/05_02_rps.py
+++ 05_functions/05_02_rps.py
@@ -3,35 +3,55 @@ Code a game of rock paper scissors.
 
 '''
 
-
 # function to get hand based on number
 # the function should take in a number and return the string representation of the hand
-def get_hand(hand):
-    # 0 = scissor, 1 = rock, 2 = paper
 
-    # for example if the variable hand is 0, it should return the string "scissor"
-    pass
+def get_hand(int):
+    if int == 0:
+        hand = "scissor"
+    elif int == 1:
+        hand = "rock"
+    else:
+        hand = "paper"
+    return(hand)
 
 # function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
-def determine_winner(computer, player):
-    pass
-
-
-
+def determine_winner(computer, user):
+    if computer == 'scissor' and user == 'rock':
+        result = "You lost :("
+    elif computer == 'paper' and user == 'scissor':
+        result = "You lost :("
+    elif computer == 'rock' and computer == 'paper':
+        reulst = "You lost :("
+    elif computer == user:
+        result = "It was a tie!!"
+    else:
+        result = "You won!"
+    return(result)
 '''
 Start here
 '''
 
 # take in a number 0-2 from the user that represents their hand
+userhand = int(input("Please enter your choice, 0 = scissor, 1 = rock, 2 = paper: "))
 
 # generate a random number 0-2 to use for the computer's hand
+from random import *
+computerhand = randint(0,2)
 
 # call the function get_hand to get the string representation of the user's hand
+get_hand(userhand)
 
 # call the function get_hand to get the string representation of the computer's hand
+get_hand(computerhand)
 
 # call the function determine_winner to figure out the winner
+determine_winner(computerhand, userhand)
 
 # print out the player hand and computer hand
+print(f"You chose {get_hand(userhand)}, and the computer chose {get_hand(computerhand)}.")
 
 # print out the winner
+print(determine_winner(computerhand, userhand))
+
+
