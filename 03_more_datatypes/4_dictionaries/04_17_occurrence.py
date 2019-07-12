d522f295bd48be130692d7d5390d77ee07ffc269
Michael Stresing <michaelstresing@michaels-mbp-2.home>
2019-07-12 21:18:31 +0100

    Last bit of 03

--- 03_more_datatypes/4_dictionaries/04_17_occurrence.py
+++ 03_more_datatypes/4_dictionaries/04_17_occurrence.py
@@ -9,3 +9,11 @@ result = {"h": 1, "e": 1, "l": 2, "o": 1}
 
 userput = input("Please enter some words: ")
 
+inputtup = tuple(userput)
+
+result_dict = {}
+
+for i in inputtup:
+    result_dict.update({i: inputtup.count(i)})
+
+print(result_dict)
--- 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
+++ 03_more_datatypes/4_dictionaries/04_19_dict_tuples.py
@@ -5,3 +5,16 @@ input_dict = {"item1": 5, "item2": 6, "item3": 1}
 result_list = [("item3", 1), ("item1", 5), ("item2", 6)]
 
 '''
+input_dict = {"item1": 5, "item2": 6, "item3": 1}
+
+resulta = input_dict.items()
+print(resulta)
+
+resultb = []
+
+for item in resulta:
+    resultb.append(item)
+
+print(resultb)
+
+#Surely there's a more sophisticated solution to this....
