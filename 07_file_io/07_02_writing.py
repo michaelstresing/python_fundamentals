3794cec57ba8a0ac4c699028182356146b177419
michaelstresing <mstresing@gmail.com>
2019-08-03 18:00:27 +0100

    file io finish

--- 07_file_io/07_02_writing.py
+++ 07_file_io/07_02_writing.py
@@ -2,3 +2,19 @@
 Write a script that reads in the contents of words.txt and writes the contents in reverse
 to a new file words_reverse.txt.
 '''
+
+wordlist = []
+
+with open('words.txt', 'r') as fin:
+        for word in fin:
+            word = word.rstrip()
+            wordlist.append(word)
+
+# print(wordlist)
+
+wordlist.reverse()
+
+with open('reversewords.txt', 'w') as fout:
+        for word in wordlist:
+            fout.write(f"{word}\n")
+
