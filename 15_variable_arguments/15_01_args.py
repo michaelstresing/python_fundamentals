1da3006cd598f8ae5b6cdf6d8d270f15f38a9343
michaelstresing <mstresing@gmail.com>
2019-08-07 23:12:58 +0100

    Args and Kwargs"

--- 15_variable_arguments/15_01_args.py
+++ 15_variable_arguments/15_01_args.py
@@ -2,3 +2,18 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+
+def greeter(*args):
+    print("Hello!", args)
+
+
+names = ["Michael"]
+
+greeter(*names)
+
+#removing comma syntax around on the output?
+# f"Hello! {args}" returns the same
+
+#adding *names (vs just 'names') removes the []... likewise interesting to note that the args seems to function
+# with or without * but calling kwargs without the ** fails...
--- 15_variable_arguments/15_02_kwargs.py
+++ 15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,16 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def order_dish(**kwargs):
+    for k, v in kwargs.items():
+        print("{} {}".format(k, v))
+
+menu = {
+    'Dominos' : "Pizza",
+    'McDonalds' : "BigMac",
+    'KFC' : "Chicken"
+}
+
+order_dish(**menu)
