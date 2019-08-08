6982fbb6f7ebea70e9798ac27aa4784bffbc3687
michaelstresing <mstresing@gmail.com>
2019-08-08 21:02:23 +0100

    enumerate work

--- 13_aggregate_functions/13_01_enumerate.py
+++ 13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,21 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+
+#from the python docs on how it functions:
+
+def enum_function(sequence, start=0):
+    n = start
+    for elem in sequence:
+        yield n, elem
+        n += 1
+
+#demonstration
+
+mydinner = ['Peas', 'Potatoes', 'Carrots', 'Steak']
+
+iate1 = enumerate(mydinner, start=1)
+
+iate2 = enum_function(mydinner, start=1)
+
+print(list((iate1)), list(iate2))
--- 13_aggregate_functions/13_02_sum.py
+++ 13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,11 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+
+def sum(*args):
+    result = 1
+    for x in args:
+        result += x
+    return result
+
+print(sum(1, 5, 2))
