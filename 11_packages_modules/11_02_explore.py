1d33eee7acc0de7fee17dead538978d77898d372
michaelstresing <mstresing@gmail.com>
2019-08-11 22:13:48 +0100

    Playing around with time

--- 11_packages_modules/11_02_explore.py
+++ 11_packages_modules/11_02_explore.py
@@ -3,7 +3,7 @@ Do some research on other popular python packages and what the are used for. Fee
 and play around a little.
 
 '''
-
+'''
 numpy *
 Pandas
 
@@ -20,7 +20,35 @@ beautifulsoup *
 
 sqlalchemy *
 
+'''
+
 #'Package' is a python file with a setup.py file in it. (Can be pip installed).
 #'Module' is a class or file within the package you can import.
 
-math datetime time (inside standard library)
+#math datetime time (inside standard library)
+
+
+import datetime
+
+d = datetime.date(1990, 9, 9)
+print(d)
+#do not use the hanging 0 infront of the month or day entries
+
+d = datetime.date.today()
+print(d)
+
+d = datetime.datetime.today()
+print(d)
+
+t = datetime.time(12, 2, 15)
+print(t)
+
+d = datetime.date.today().weekday()
+print(d)
+#isoweekday vs. weekday - unclear why one over other?
+
+d = datetime.timezone.utc
+print(d)
+
+help(datetime.timezone)  #https://julien.danjou.info/python-and-timezones/
+
